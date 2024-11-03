d1297f09492ac100a93b06573e5301ef87983fb1
Eric Vaz <10699597+ericvaz@users.noreply.github.com>
2024-11-03 11:17:01 -0500

    Create Nov1.py

---
+++ Nov1/Nov1.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import geopandas as gpd
+from pyproj import Transformer
+
+earthquakes = pd.read_csv(your_file_path_here/Eartquakes-1990-2023.csv)
